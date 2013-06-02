c67dead43771a2d2bfa93a5c09975d655ad33df5
Johannes Maximilian Toball <code@gedankenacker.de>
2013-06-02 02:42:03 +0200

    Basic Command/Path Completions work fine

--- pymenu.py
+++ pymenu.py
@@ -1,18 +1,39 @@
 import os
+import stat
 
-class PyMenu:
+class PyMenuCompletions:
 
     path = "."
+    binpaths = os.environ['PATH'].split(':')
+
+    def getExecutables(self, paths):
+        executables = []
+        executable = stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH
+        for dir in paths:
+            for filename in os.listdir(dir):
+                path = os.path.join(dir,filename)
+                if os.path.isfile(path) and os.stat(path).st_mode & executable:
+                    executables.append(filename)
+        return executables
+
+    def getAllExecutables(self):
+        return self.getExecutables(self.binpaths)
+
+    def completeCommand(self, name):
+        commandfilter = lambda x: x.startswith(name)
+        return filter(commandfilter, self.getAllExecutables())
 
     def completePath(self, path):
         head, tail = os.path.split(path)
-        print "---"
-        print "head:"+ head
-        print "tail:"+ head
-        print "completed: "+ os.path.join(self.path, head)
-        print "---"
         fullPath = self.path
         if head:
             fullPath = os.path.join(self.path, head)
         pathfilter = lambda x: x.startswith(tail)
         return filter(pathfilter, os.listdir(fullPath))
+
+    def complete(self, tocomplete):
+        completion = self.completeCommand(tocomplete)
+        if completion:
+            return completion
+        else:
+            return self.completePath(tocomplete)
--- tests.py
+++ tests.py
@@ -1,20 +1,59 @@
 import unittest
+import os
 
-from pymenu import PyMenu
+from pymenu import PyMenuCompletions
 
 class TestPathCompletions(unittest.TestCase):
 
     def setUp(self):
-        self.pymenu = PyMenu()
+        self.pymenu = PyMenuCompletions()
 
     def test_files(self):
         self.assertEqual(self.pymenu.completePath("t"), ["tests.py", "tests"])
 
     def test_dirs(self):
-        self.assertEqual(self.pymenu.completePath("tests/"), ["test1", "test2"])
+        self.assertEqual(self.pymenu.completePath("tests/t"), ["test1", "test2"])
 
     def test_nonexistent(self):
         self.assertEqual(self.pymenu.completePath("xyz"), [])
+
+
+class TestOSCommandCompletion(unittest.TestCase):
+
+    def setUp(self):
+        self.pymenu = PyMenuCompletions()
+
+    # dir is hopefully always available on all target platforms,
+    # although its functionality might be different
+    def test_name(self):
+        self.assertTrue("dir" in self.pymenu.completeCommand("di"))
+
+
+class TestCommandCompletion(unittest.TestCase):
+
+    def setUp(self):
+        self.pymenu = PyMenuCompletions()
+        self.pymenu.binpaths = [os.path.join(os.path.dirname(__file__), "tests")]
+
+    def test_name(self):
+        self.assertEqual(self.pymenu.completeCommand("di"), ["dir"])
+
+    def test_nonexitent(self):
+        self.assertFalse("xyzlolo" in self.pymenu.completeCommand("xyzlolo"))
+
+
+class TestFallbacks(unittest.TestCase):
+
+    def setUp(self):
+        self.pymenu = PyMenuCompletions()
+        self.pymenu.path = os.path.join(os.path.dirname(__file__), "tests")
+        self.pymenu.binpaths = [self.pymenu.path]
+
+    def test_fallnofallback(self):
+        self.assertEqual(self.pymenu.complete("dir"), ["dir"])
+
+    def test_fallbacktofile(self):
+        self.assertEqual(self.pymenu.complete("test"), ["test1", "test2"])
 
 if __name__ == '__main__':
     unittest.main()
