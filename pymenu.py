4ecb0054c08dc3aaae6b325ce0dea76e79b8e769
Johannes Maximilian Toball <code@gedankenacker.de>
2013-06-02 06:12:58 +0200

    First Steps to a GUI with Tkinter

--- pymenu.py
+++ pymenu.py
@@ -14,12 +14,13 @@ class PyMenuCompletions:
                 path = os.path.join(dir,filename)
                 if os.path.isfile(path) and os.stat(path).st_mode & executable:
                     executables.append(filename)
-        return executables
+        return set(executables)
 
     def getAllExecutables(self):
         return self.getExecutables(self.binpaths)
 
     def completeCommand(self, name):
+        #TODO Regex-Matching
         commandfilter = lambda x: x.startswith(name)
         return filter(commandfilter, self.getAllExecutables())
 
@@ -28,6 +29,7 @@ class PyMenuCompletions:
         fullPath = self.path
         if head:
             fullPath = os.path.join(self.path, head)
+        #TODO Regex-Matching
         pathfilter = lambda x: x.startswith(tail)
         return filter(pathfilter, os.listdir(fullPath))
 
@@ -37,3 +39,39 @@ class PyMenuCompletions:
             return completion
         else:
             return self.completePath(tocomplete)
+
+
+from tkinter import *
+
+class PyMenuGUI:
+
+    def __init__(self):
+        self.root = Tk()
+        self.frame = Frame(self.root, bg='black', width=self.root.winfo_screenwidth(), height=25)
+        self.frame.pack_propagate(False)
+        self.frame.pack()
+
+        self.prompt = Entry(self.frame)
+        self.prompt.focus_set()
+        self.prompt.pack(side=LEFT)
+
+        self.completions = Frame(self.frame, bg='black')
+
+        self.refill(model.completeCommand('dir'))
+        self.root.overrideredirect(True)
+        self.root.focus_set()
+        self.root.bind("<Escape>", lambda e: e.widget.quit())
+        self.root.mainloop()
+
+    def refill(self, completions):
+        for completion in self.completions.winfo_children():
+            completion.destroy()
+
+        for completion in completions:
+            label = Label(self.completions, fg='white', text=completion)
+            label.pack(side=LEFT, padx=5)
+        self.completions.pack(side=LEFT, padx=25)
+
+if __name__ == '__main__':
+    model = PyMenuCompletions()
+    gui   = PyMenuGUI()
